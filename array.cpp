801da55f4de45eee7c202f79b2fbb8212dd372e9
Your Name <freeluck.v.i.p999@gmail.com>
2026-01-27 16:45:22 +0000

    fixed bug final edit of array cpp

--- array.cpp
+++ array.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 // program to calculate total of array
-
+// final edit of this program
 int array(int array[], int size) {
   int arrayreturn = 0;
 
-  for(int i = 0; i < SIZE; i++){
+  for(int i = 0; i < size; i++){
     std::cout << "Enter number:\n";
     std::cin >> array[i];
     arrayreturn += array[i];   
