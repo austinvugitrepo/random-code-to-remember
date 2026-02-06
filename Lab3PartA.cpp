c440989211d55334f3a4f5d996d1db73b32d2f89
Your Name <freeluck.v.i.p999@gmail.com>
2026-02-06 16:10:58 +0000

    filling 2d array and 1d array with equation program

---
+++ Lab3PartA.cpp
@@ -0,0 +1,43 @@
+//austin vu
+// 2d array and 1d array diagonal
+#include <iostream>
+
+int darray(int array_name[6][6], int row, int col);
+
+int main(){
+
+int row = 6;
+int col = 6;
+int array_name[6][6l];
+
+darray(array_name, row , col);
+
+return 0;
+}
+
+int darray(int array_name[6][6], int row, int col){
+
+  for (int i = 0; i < row; i++){
+    for (int j = 0; j < col; j++){
+    array_name[i][j] = i + j + 2;
+    }
+  
+  }
+ std::cout << "2D array:\n";
+ for (int i = 0; i < row; i++){
+    for (int j = 0; j < col; j++){
+     std::cout << array_name[i][j] << "  ";
+    }
+    std::cout << std::endl;  
+  }
+
+std::cout << std::endl;  
+ std::cout << "1D array:\n";
+ for (int i = 0; i < row; i++){
+    std::cout << array_name[i][i] << "  ";
+  }
+std::cout << std::endl; 
+
+  return 0;
+}
+
