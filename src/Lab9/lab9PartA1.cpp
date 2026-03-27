bb27af59fed3067ccfa5ba72476a49f40035b1f1
Austin Vu <freeluck.v.i.p999@gmail.com>
2026-03-27 15:28:51 +0000

    will debug part A1 later because , is not being appended to nameArranger properly

--- src/Lab9/lab9PartA1.cpp
+++ src/Lab9/lab9PartA1.cpp
@@ -4,22 +4,28 @@
 
 int main(){
 
-char firstName[50];
-char middleName[50]; //char arrays are strings
-char lastName[50];
+int csize = 50;
+char firstName[csize];
+char middleName[csize]; //char arrays are strings
+char lastName[csize];
+char nameArranger[csize * 3]; // 3 times the size of the char arrays above
 
 printf("What is your first name?: "); //ask for names
 fgets(firstName, sizeof(firstName), stdin); // syntax is fgets(arr, size of array, and input aka std input from keyboard)
+//fgets includes the \n at the end so we need to get rid of it for cleaner output when appending string at the end
+firstName[strcspn(firstName, "\n")] = '\0'; //strcspn (string complement span aka finds the thing not in my array via the second slot which can be a string or a different complementary array) , then make that position null to end the string 
 
 printf("What is your middle name?: ");
 fgets(middleName, sizeof(middleName), stdin);
 
 printf("What is your last name?: ");
 fgets(lastName, sizeof(lastName), stdin);
+firstName[strcspn(lastName, "\n")] = '\0';
 
-//debug print 
-
-printf("Your first name is: %s", firstName); // %s stands for string 
+strcpy(nameArranger, lastName); //copy lastname into nameArranger 
+strcat(nameArranger, ","); // append "," to the last name
+printf("Your last name is: %s", nameArranger);
+printf("\n");
 
 
 return 0;
