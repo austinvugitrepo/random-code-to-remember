bbc9d653a1af95a7ca539b1db832ffd8c7758497
austin <austin@root-user.org>
2026-08-26 13:28:59 -0500

    made my function definitions

--- src/dlab1/lab1parta.cpp
+++ src/dlab1/lab1parta.cpp
@@ -10,13 +10,13 @@ class secretType {
 		void
 		print();
 		void
-		setName();
+		setName(std::string);
 		void
-		setAge();
+		setAge(int);
 		void
-		setWeight();
+		setWeight(int);
 		void
-		setHeight();
+		setHeight(double);
 		std::string
 		getName();
 		int
@@ -25,7 +25,92 @@ class secretType {
 		getWeight();
 		double
 		getHeight();
+		secretType();
+		secretType(std::string, int, int, double,);
 
 };
 
 
+void
+secretType :: print()
+{
+	std::cout << "Name: " << getName() << endl;
+	std::cout << "Age: " << getAge() << " yrs\n";
+	std::cout << "Weight: " << getWeight() << "lbs\n";
+	std::cout << "Height" << getHeight() << "ft\n";
+
+}
+
+void
+secretType :: setName(std::string n)
+{
+	name = n;
+
+} 
+
+void
+secretType :: setAge(int a)
+{
+	age = a;
+
+}
+
+void
+secretType :: setWeight(int w)
+{
+	weight = w;
+
+}
+
+void
+secretType :: setHeight(double h)
+{
+	height = h;
+
+}
+
+std::string
+secretType :: getName()
+{
+	return name;
+
+}
+
+int
+secretType :: getAge()
+{
+	return age;
+
+}
+
+int
+secretType :: getWeight()
+{
+	return weight;
+
+}
+
+double
+secretType :: getHeight()
+{
+	return height;
+
+} 
+
+secretType :: secretType()
+{
+	name = "";
+	age = 0;
+	weight = 0;
+	height = 0.0;
+
+} 
+
+secretType :: secretType(std::string n, int a, int w, double h) 
+{
+	name = n;
+	age = a;
+	weight = w;
+	height = h;
+
+}
