646570eb6ebe64bda21d40d03c415f8b79262097
austin <austin@root-user.org>
2026-08-26 12:44:51 -0500

    destroyed my src file cuz of a risky shell redirection, really need to version control.

---
+++ src/dlab1/lab1parta.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class secretType {
+	private:
+		std::string name;
+		int age, weight;
+		double height;
+	public:
+		void
+		print();
+		void
+		setName();
+		void
+		setAge();
+		void
+		setWeight();
+		void
+		setHeight();
+		std::string
+		getName();
+		int
+		getAge();
+		int
+		getWeight();
+		double
+		getHeight();
+
+};
+
+
