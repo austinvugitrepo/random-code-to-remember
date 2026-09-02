18b89dfbbc2e8a5e371a846416ec5410456704f4
austin <austin@root-user.org>
2026-09-02 11:16:35 -0500

    work in progress for class Animal member function definiton

---
+++ src/dlab2/2a.cpp
@@ -0,0 +1,62 @@
+#include "lab2parta.h"
+
+void
+Animal :: setName(std::string n)
+{
+	name = n;
+
+}
+
+void
+Animal :: setNumLegs(int l)
+{
+	numLegs = l;
+
+}
+
+std::string
+Animal :: getName()
+{
+	return name;
+
+}
+
+int
+Animal :: getNumLegs()
+{
+	return numLegs;
+
+}
+
+std::string
+Animal :: speak()
+{
+
+
+} 
+
+std::string
+Animal :: move()
+{
+
+
+} 
+
+void
+Animal :: print()
+{
+
+
+}
+
+Animal :: Animal()
+{
+
+
+}
+
+Animal :: Animal(std::string n, int l)
+{
+
+
+}
