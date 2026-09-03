bda97a67df57dcfd2c027f11f49abad8b96754ed
austin <austin@root-user.org>
2026-09-03 11:28:24 -0500

    finished part a

--- src/dlab2/2a.cpp
+++ src/dlab2/2a.cpp
@@ -49,8 +49,6 @@ Animal :: print()
 {
 	std::cout << "My name is " << getName() << std::endl;
 	std::cout << "I have " << getNumLegs() << " legs.\n"; 
-	std::cout << speak();
-	std::cout << move();
 }
 
 Animal :: Animal()
--- src/dlab2/lab2parta.cpp
+++ src/dlab2/lab2parta.cpp
@@ -9,8 +9,14 @@ main()
 	Dog d(4, "Dog");
 
 	a.print();
+	std::cout << a.speak();
+	std::cout << a.move();
 	c.print();
+	std::cout <<  c.speak();
+	std::cout << c.move();
 	d.print();
+	std::cout << d.speak();
+	std::cout << d.move();
 
 	return 0;
 
