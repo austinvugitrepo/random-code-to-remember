4a4ff0d5096cec6619adb6e5f99bf8362cea1c29
austin <austin@root-user.org>
2026-09-09 23:15:13 -0500

    should be done with the member function definitions of the classes

---
+++ src/dlab3/3a.cpp
@@ -0,0 +1,33 @@
+#include "lab3parta.h"
+
+void
+Polygon :: set_values(int w, int h) 
+{
+	width = w; 
+	height = h; 
+
+}
+
+int
+Polygon :: area()
+{
+	int area = 0;
+	return area;
+
+}
+
+int
+Triangle :: area()
+{
+	int area = w * h * 0.5;
+	return area;
+
+}
+
+int
+Rectangle :: area()
+{
+	int area = w * h
+	return area;
+
+}
