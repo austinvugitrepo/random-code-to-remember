ebe4929e82d8b1e5c891abc2cbc3249f5524fae6
Austin Vu <austin@root-user.org>
2026-09-18 01:46:49 +0000

    improved triangle.cpp

--- src/misc/triangle.cpp
+++ src/misc/triangle.cpp
@@ -1,101 +1,131 @@
 #include <iostream>
-using namespace std;
 
-class triangle{
+class triangle {
 
-private:
+	private:
 
-double base;
-double height;
+		double base;
+		double height;
 
-public:
+	public:
 
-triangle(){
-  
-  base =0;
-  height =0;
-}
-triangle(double b, double h){
-  
-  base = b;
-  height = h;
-}
+		triangle();
+		triangle(double, double);
 
-void setbase(double);
-void setheight(double);
+		void
+		setbase(double);
+		void
+		setheight(double);
 
-double getbase();
-double getheight();
-double getarea();
+		double
+		getbase();
+		double
+		getheight();
+		double
+		getarea();
 };
 
-void triangle::setbase(double b){
-  
-  base = b;
-}
-void triangle::setheight(double h){
-  
-  height = h;
-}
-double triangle::getbase(){
-  
-  return base;
-}
-double triangle::getheight(){
-  
-  return height;
+triangle :: triangle()
+{
+	base = 0;
+  	height = 0;
 }
 
-double triangle::getarea(){
-  
-  return (base*height*.5);
+triangle :: triangle(double b, double h)
+{
+	base = b;
+	height = h;
 }
 
-int main() { 
-  
-  int amount;
-  
-  cout << "How many triangles do you want to make?" << endl;
-  cin >> amount;
-  
-  triangle array[amount];
+void
+triangle::setbase(double b)
+{
+	base = b;
+}
 
-  for (int i = 0; i < amount; i++){
-    
-    int base = 0;
-    int height = 0;
-    
-    cout << "what is the base of triangle " << i+1<< ": " << endl;
-    cin >>base;
-    
-    cout << "what is the height of triangle " << i+1 << ": " << endl;
-    cin >>height;
-    
-    array[i].setbase(base);
-    array[i].setheight(height);
-    
-  }
+void
+triangle::setheight(double h)
+{
+	height = h;
+}
 
-  if(amount == 1){
-    
-    cout << "The triangle with the highest area is triangle 1" << endl;
-    cout << "The height is " << array[0].getheight() << endl;
-    cout << "The base is " << array[0].getbase() << endl;
-    cout << "The area of triangle 1 is " << array[0].getarea() << endl;
-  }
+double
+triangle::getbase()
+{
+	return base;
+}
 
-  for (int i = 1, highest = 0; i < amount; i++){
+double
+triangle::getheight()
+{
+	return height;
+}
+
+double
+triangle::getarea()
+{
+	return base * height * 0.5;
+}
+
+int main()
+{ 
+	int amount = 0;
+  
+	std::cout << "How many triangles do you want to make?" << std::endl;
+	std::cin >> amount;
+	
+	if (amount <= 0) { 
+
+		std::cout << "Can't make that amount of triangles!" << std::endl;
+		return 1;
+	}
+
+	triangle *array = new triangle[amount];
+
+	for (int i = 0; i < amount; i++) {
     
-    if(array[i].getarea() > array[highest].getarea()){
-      
-      highest = i;
-    }
-    if(i+1 == amount){
-      
-      cout << "The triangle with the highest area is triangle " << highest +1 << endl;
-      cout << "The area of triangle " << highest +1 << " is " << array[highest].getarea()<< endl;
-      
-    }
-  }
+		int base = 0;
+		int height = 0;
+    
+		std::cout << "what is the base of triangle " << i + 1 << ": " << std::endl;
+		std::cin >> base;
+    
+		std::cout << "what is the height of triangle " << i + 1 << ": " << std::endl;
+		std::cin >> height;
+    
+    		array[i].setbase(base);
+    		array[i].setheight(height);
+    
+  	}
+
+	if (amount == 1) {
+    
+		std::cout << "The triangle with the highest area is triangle 1" << std::endl;
+		std::cout << "The height is " << array[0].getheight() << std::endl;
+		std::cout << "The base is " << array[0].getbase() << std::endl;
+		std::cout << "The area of triangle 1 is " << array[0].getarea() << std::endl;
+
+		return 0;
+	}
+
+	int highest = 0;
+
+	for (int i = 1; i < amount; i++) {
+    
+		if (array[i].getarea() > array[highest].getarea()) {
+
+			highest = i;
+		}
+
+  	}
+    
+	std::cout << "The triangle with the highest area is triangle " << highest + 1 << std::endl;
+	std::cout << "The height is " << array[highest].getheight() << std::endl;
+	std::cout << "The base is " << array[highest].getbase() << std::endl;
+	std::cout << "The area of the largest triangle is " << array[highest].getarea() << std::endl;
+
+	delete [] array;
+
+	return 0;
 
 }
